338f8006f811998efb5da51c72fef8892de11233
julienbrosseau <jbrosseau85@gmail.com>
2019-11-15 15:29:45 -0500

    MàJ "traitement.py" (completer les donnees manquantes, remplacer tout par des donnees numeriques)

--- bin/traitement.py
+++ bin/traitement.py
@@ -1,28 +1,40 @@
 # Traitement de la base de donnees
 
-def set_age_data(data):
-    # Completer les donnees manquantes pour les ages
+def traitement(data):
+    # Retirer les variables qui ne nous interessent pas pour la classification -----
+    data = data.drop(['PassengerId', 'Ticket', 'Cabin', 'Name'], axis=1)
+    # ------------------------------------------------------------------------------
+
+    # Remplacer les "string" par des "int" pour le sexe ----------------------------
+    data.loc[data['Sex']=='male','Sex'] = 1
+    data.loc[data['Sex']=='female','Sex'] = 0
+    # ------------------------------------------------------------------------------
+
+    # Completer les donnees manquantes pour les ages -------------------------------
     for sexe in range(2):
         for classe in range(3):
             # Recuperer la mediane en fonction du sexe et de la classe
             somme_age = data[(data['Sex'] == sexe) & (data['Pclass'] == classe+1)]['Age'].dropna()
             mediane_age = somme_age.median()
 
-            # Attribuer l age median aux donnees manquantes
-            data.loc[(data.Age.isnull()) & (data.Sex == sexe) & (data.Pclass == classe+1),'Age'] = mediane_age
+            # Attribuer l'age median aux donnees manquantes en fonction du sexe et de la classe
+            data.loc[(data.Age.isnull()) & (data.Sex == sexe) & (data.Pclass == classe+1), 'Age'] = mediane_age
 
-    # Remplacer les "float" par des "int"
+    # Remplacer les "float" par des "int" pour les ages
     data['Age'] = data['Age'].astype(int)
-    return data
+    # ------------------------------------------------------------------------------
 
-def traitement(data):
-    # Retirer les variables qui ne nous interessent pas pour la classification
-    data = data.drop(['PassengerId', 'Ticket', 'Cabin', 'Name'], axis=1)
+    # Completer les donnees manquantes pour les ports d'embarqations ---------------
+    # Assigner le port le plus frequent (apres analyse des donnees) pour les donnees manquantes
+    data.loc[data.Embarked.isnull(), 'Embarked'] = 'S'
 
-    # Remplacer les "string" par des "int"
-    data.loc[data['Sex']=='male','Sex'] = 1
-    data.loc[data['Sex']=='female','Sex'] = 0
+    # Remplacer les "string" par des "int" pour les ports
+    data['Embarked'] = data['Embarked'].map({'S':0, 'C':1, 'Q':2})
+    # ------------------------------------------------------------------------------
 
-    data = set_age_data(data)
+    # Completer les donnees manquantes pour le prix du billet ----------------------
+    # Assigner le tarif le plus frequent (apres analyse des donnees) pour les donnees manquantes
+    data.loc[data.Fare.isnull(), 'Fare'] = 7.895800
+    # ------------------------------------------------------------------------------
 
     return data
--- tests/test_traitement.py
+++ tests/test_traitement.py
@@ -8,5 +8,9 @@ data_train = op.get_training_data()
 data_test  = op.get_testing_data()
 
 data_train = tr.traitement(data_train)
+data_test  = tr.traitement(data_test)
 
-print(op.get_head_data(data_train))
+#print(op.get_head_data(data_train))
+print(op.get_null_data(data_train))
+print(op.get_null_data(data_test))
+#print(op.get_describe_data(data_test))
