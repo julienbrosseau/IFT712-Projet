1b48e4935a2e6aebf9f2a5b9a63c72bfb1b5aea2
julienbrosseau <jbrosseau85@gmail.com>
2019-11-16 19:37:22 -0500

    (erratum) MàJ du modele apres les tests

--- bin/regression_logistique.py
+++ bin/regression_logistique.py
@@ -20,10 +20,6 @@ def predict(x_train):
     # Retourne la prediction des donnees
     return logistic_regression.predict(x_train)
 
-def predict_proba(x_train):
-    # Retourne la probabilite de chaque classe des donnees
-    return logistic_regression.predict_proba(x_train)
-
 def score(x_train, t_train):
     # Retourne la score moyen des donnees en fonction de leur classe
     return logistic_regression.score(x_train, t_train)
