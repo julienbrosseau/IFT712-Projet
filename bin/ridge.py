dbfa63ec515b88f94846206b642f333fd9fce62e
julienbrosseau <jbrosseau85@gmail.com>
2019-11-19 16:53:57 -0500

    MàJ modification de la structure (forme "class")

--- bin/ridge.py
+++ bin/ridge.py
@@ -4,20 +4,27 @@
 # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.RidgeClassifierCV.html
 
 from sklearn.linear_model import RidgeClassifierCV
+import numpy as np
 
-ridge = RidgeClassifierCV(
-    alphas   = [1e-3, 1e-2, 1e-1, 1],
-    cv       = 5
-)
+class Ridge():
+    def __init__(self):
+        # Initalisation du modele
+        self.ridge = RidgeClassifierCV(
+            alphas   = np.array(np.arange(1e-6, 1e-3, 1e-4)),
+            cv       = 5
+        )
 
-def fit(x_train, t_train):
-    # Retroune l entrainement du modele par rapport aux donnees 
-    return ridge.fit(x_train, t_train)
+    def fit(self, x_train, t_train):
+        # Retroune l entrainement du modele par rapport aux donnees 
+        return self.ridge.fit(x_train, t_train)
 
-def predict(data):
-    # Retourne la prediction des donnees
-    return ridge.predict(data)
+    def predict(self, data):
+        # Retourne la prediction des donnees
+        return self.ridge.predict(data)
 
-def score(x_train, t_train):
-    # Retourne la score moyen des donnees en fonction de leur classe
-    return ridge.score(x_train, t_train)
+    def score(self, x_train, t_train):
+        # Retourne la score moyen des donnees en fonction de leur classe
+        return self.ridge.score(x_train, t_train)
+
+    def get_best_param(self):
+        return self.ridge.alpha_
--- tests/test_ridge.py
+++ tests/test_ridge.py
@@ -9,13 +9,17 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
 # Récupération des donnees
-data_train = op.get_training_data()
-data_test  = op.get_testing_data()
-data_ref   = op.get_referencing_data()
+data_opening = op.DataOpening()
+
+data_train = data_opening.get_training_data()
+data_test  = data_opening.get_testing_data()
+data_ref   = data_opening.get_referencing_data()
 
 # Traitement des donnees
-data_train = tr.traitement(data_train)
-data_test  = tr.traitement(data_test)
+treatment = tr.Treatment()
+
+data_train = treatment.data_treatment(data_train)
+data_test  = treatment.data_treatment(data_test)
 
 # Affiliation des donnees
 x_train = data_train.drop(["Survived"], axis=1)
@@ -24,18 +28,22 @@ x_test  = data_test
 t_test  = data_ref["Survived"]
 
 # Classification par ridge
+ridge = rd.Ridge()
+
 # Entrainement des donnees
-rd.fit(x_train, t_train)
-predic_train = rd.predict(x_train)
+ridge.fit(x_train, t_train)
+predic_train = ridge.predict(x_train)
 
 # Test de l'entrainement du modele
-rd.fit(x_test, t_test)
-predic_test = rd.predict(x_test)
+ridge.fit(x_test, t_test)
+predic_test = ridge.predict(x_test)
 
 # Affichage des donnees en fonction de leur classification
 # Affichage erreurs pour l'entrainement et les tests
-print("Erreur d'entrainement :", (1 - rd.score(x_train, t_train)) * 100, "%")
-print("Erreur de test :", (1 - rd.score(x_test, t_test)) * 100, "%")
+print("Erreur d'entrainement :", (1 - ridge.score(x_train, t_train)) * 100, "%")
+print("Erreur de test :", (1 - ridge.score(x_test, t_test)) * 100, "%")
+
+print("Meilleur hyperparametre :", ridge.get_best_param())
 
 # Affichage matrice de confusion
 sns.heatmap(confusion_matrix(t_train, predic_train),annot=True,lw =2,cbar=False)
