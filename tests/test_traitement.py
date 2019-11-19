45b28ad03adfd0b953f82dfa42724a7d8bf43b41
julienbrosseau <jbrosseau85@gmail.com>
2019-11-19 16:33:25 -0500

    MàJ modification de la structure (forme "class")

--- tests/test_traitement.py
+++ tests/test_traitement.py
@@ -3,10 +3,13 @@
 import bin.ouverture as op
 import bin.traitement as tr
 
-# Traitement des donnees
-data_train = op.get_training_data()
-data_test  = op.get_testing_data()
+# Ouverture des fichiers
+data_opening = op.DataOpening()
 
+data_train = data_opening.get_training_data()
+data_test  = data_opening.get_testing_data()
+
+# Traitement des donnees
 treatment = tr.Treatment()
 
 data_train = treatment.data_treatment(data_train)
@@ -16,4 +19,4 @@ data_test  = treatment.data_treatment(data_test)
 #print(op.get_null_data(data_train))
 #print(op.get_null_data(data_test))
 #print(op.get_describe_data(data_test))
-print(op.get_describe_data(data_train))
+print(data_opening.get_describe_data(data_train))
