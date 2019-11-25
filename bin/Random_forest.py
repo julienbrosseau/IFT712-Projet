866a9d6cbda0c1e269416559da5f77925de48300
Arnaud2N2 <36883495+Arnaud2N2@users.noreply.github.com>
2019-11-24 22:49:49 -0500

    Mise en place de la classe randomForest

--- bin/Random_forest.py
+++ bin/Random_forest.py
@@ -1,43 +1,39 @@
 # Classification par Random forest
 
 # Source du module "Ramdom forest"
-#https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+# https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
 
+# Source du module "GridSearchCV"
+# https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 
-clf = RandomForestClassifier(n_estimators=’warn’,
-                             criterion=’gini’,
-                             max_depth=None,
-                             min_samples_split=2,
-                             min_samples_leaf=1,
-                             min_weight_fraction_leaf=0.0,
-                             max_features=’auto’,
-                             max_leaf_nodes=None,
-                             min_impurity_decrease=0.0,
-                             min_impurity_split=None,
-                             bootstrap=True,
-                             oob_score=False,
-                             n_jobs=None,
-                             random_state=None,
-                             verbose=0,
-                             warm_start=False,
-                             class_weight=None)
+class randomForest():
+    def __init__(self):
+        #Initialisation du module
+        grid_parameters = {'n_estimators': range(1, 20, 1)}
 
-def fit(x_train, y_train):
-     # Retourne l'entrainement du modele par rapport aux donnees
-     return clf.fit(x_train, y_train)
+        self.randomForest = GridSearchCV(randomForestClassifier(
+            n_estimators=100, 
+            max_depth=2,
+            random_state=0
+        ), grid_parameters, cv=15, iid=False) 
+
+    def fit(self, x_train, y_train):
+        # Retourne l'entrainement du modele par rapport aux donnees
+        return self.fit(x_train, y_train)
+        
+    def predict(self, x_train):
+        # Retourne la prediction des donnees
+        return self.predict(x_train)
     
-def predict(x_train):
-    # Retourne la prediction des donnees
-    return clf.predict(x_train)
-
-def get_best_param(x_train):
-    # Retourne les meilleurs paramètres
-    return clf.get_best_param(x_train)
-
-def score(x_train, y_train):
-    # Retourne les vecteurs de support
-    return clf.support_vectors_(x_train, y_train)
-
+    def score(self, x_train, y_train):
+        # Retourne les vecteurs de support
+        return self.randomForest(x_train, y_train)
+    
+    def get_best_param(self):
+        # Retourne le meilleur hyperparametre
+        return self.get_params()
+    
