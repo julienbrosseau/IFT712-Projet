79bfa1ac8c49eeddad5b7632cbc948d8c7550c75
Arnaud2N2 <36883495+Arnaud2N2@users.noreply.github.com>
2019-11-24 22:50:35 -0500

    Modification de test_random_forest.py

--- tests/test_random_forest.py
+++ tests/test_random_forest.py
@@ -1,6 +1,5 @@
 # Test du fichier "random_forest.py"
 
-import numpy as np
 import bin.data_opening as op
 import bin.treatment as tr
 import bin.random_forest as rf
@@ -11,31 +10,44 @@ from sklearn.metrics import confusion_matrix
 
 
 # Récupération des données
-data_train = op.get_training_data()
-data_test  = op.get_testing_data()
-data_ref   = op.get_referencing_data()
+opening = op.DataOpening()
+data_train = opening.get_training_data()
+data_test  = opening.get_testing_data()
+data_ref   = opening.get_referencing_data()
 
 # Traitement des donnees
-data_train = tr.treatment(data_train)
-data_test  = tr.treatment(data_test)
+treatment = tr.Treatment()
+data_train = treatment.data_treatment(data_train)
+data_test  = treatment.data_treatment(data_test)
 
-# Affiliation des donnees
-X_train = data_train.drop(["Survived"], axis=1)
-Y_train = data_train["Survived"]
-X_test  = data_test
-Y_test = data_ref["Survived"]
+
+# Affiliation des données
+x_train = data_train.drop(["Survived"], axis=1)
+t_train = data_train["Survived"]
+x_test  = data_test
+t_test = data_ref["Survived"]
 
 
 # Classification par random forest
-rf.fit(X_train, Y_train)  
+rf = rf.randomForest()
 
-print(rf.clf.feature_importances_)
-predict_test = rf.predict(X_test)
+# Entrainement des donnees
+rf.fit(x_train, t_train)
+predic_train = rf.predict(x_train)
 
-print("Meilleur hyperparametre :", rf.get_best_param())
+# Prediction sur les donnees de test
+predic_test = rf.predict(x_test)
+
+# Affichage des donnees en fonction de leur classification
+# Affichage erreurs pour l'entrainement et les tests
+print("Erreur d'entrainment : ", (1 - rf.score(x_train, t_train)) * 100, "%")
+print("Erreur de test : ", (1 - rf.score(x_test, t_test)) * 100, "%")
+print(rf.get_best_param())
 
 # Affichage matrice de confusion
-sns.heatmap(confusion_matrix(Y_train, predict_test), annot=True, lw=2, cbar=False) #Verif!
+sns.heatmap(confusion_matrix(t_test, predic_test), annot=True, lw =2, cbar=False)
 
 plt.title("Matrice de confusion")
-plt.ylabel("Valeurs reelles")
+plt.ylabel("Valeurs réelles")
+plt.xlabel("Valeurs prédis")
+plt.show()
