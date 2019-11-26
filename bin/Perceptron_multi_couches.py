e0e4832c696b30fc2fa0ef93494366405028db23
Arnaud2N2 <36883495+Arnaud2N2@users.noreply.github.com>
2019-11-26 16:09:06 -0500

    Passage du fichier sous forme de classe

--- bin/Perceptron_multi_couches.py
+++ bin/Perceptron_multi_couches.py
@@ -4,20 +4,27 @@
 # https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html
 
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
 
-mlp = MLPClassifier(
-        hidden_layer_sizes=(25,13,3),
-        max_iter=1000
-        )
+class mlp():
+    def __init__(self):
+        #Initialisation du module
+        grid_parameters = {'n_estimators': range(1, 20, 1)}
 
-def fit(x_train, t_train):
-    # Retroune l'entrainement du modele par rapport aux donnees 
-    return mlp.fit(x_train, t_train)
+        self.mlp = GridSearchCV(MLPClassifier(
+            hidden_layer_sizes=(25,13,3),
+            max_iter=1000
+        ), grid_parameters, cv=15, iid=False) 
 
-def predict(x_train):
-    # Retourne la prediction des donnees
-    return mlp.predict(x_train)
-
-def score(x_train, t_train):
-    # Retourne la score moyen des donnees en fonction de leur classe
-    return mlp.score(x_train, t_train)
+    def fit(self, x_train, t_train):
+        # Retroune l'entrainement du modele par rapport aux donnees 
+        return self.mlp.fit(x_train, t_train)
+    
+    def predict(self, x_train):
+        # Retourne la prediction des donnees
+        return self.mlp.predict(x_train)
+    
+    def score(self, x_train, t_train):
+        # Retourne la score moyen des donnees en fonction de leur classe
+        return self.mlp.score(x_train, t_train)
+    
