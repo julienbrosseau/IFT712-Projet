cfd6358d9ae3f6dcad9bcc24e7ac4d808216f50c
Arnaud2N2 <36883495+Arnaud2N2@users.noreply.github.com>
2019-11-26 20:42:33 -0500

    Adaptation du modèle

--- tests/test_perceptron_multi_couches.py
+++ tests/test_perceptron_multi_couches.py
@@ -1,50 +1,55 @@
 # Test du fichier "perceptron_multi_couches.py"
 
-import numpy as np
 import bin.data_opening as op
 import bin.treatment as tr
 import bin.perceptron_multi_couches as mlp
 
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.metrics import log_loss
 from sklearn.metrics import confusion_matrix
 
 # Récupération des données
-data_train = op.get_training_data()
-data_test  = op.get_testing_data()
-data_ref   = op.get_referencing_data()
+data_opening = op.DataOpening()
+data_train = data_opening.get_training_data()
+data_test  = data_opening.get_testing_data()
+data_ref   = data_opening.get_referencing_data()
 
 # Traitement des donnees
+treatment = tr.Treatment()
 data_train = tr.treatment(data_train)
 data_test  = tr.treatment(data_test)
 
 # Affiliation des donnees
-X_train = data_train.drop(["Survived"], axis=1)
-Y_train = data_train["Survived"]
-X_test  = data_test
-Y_test = data_ref["Survived"]
+x_train = data_train.drop(["Survived"], axis=1)
+t_train = data_train["Survived"]
+x_test  = data_test
+t_test = data_ref["Survived"]
 
 # Classification par perceptron mutli-couches
+mlp = mlp.Mlp()
+
 # Entrainement des donnees
-print(mlp.fit(X_train, Y_train))
-train_acc_mlp = round(mlp.score(X_train, Y_train) * 100, 2)
-print(train_acc_mlp)
+mlp.fit(x_train, t_train)
+predict_train = mlp.predict(x_train)
 
-# Test d'évaluation de la précision
-Y_pred_mlp = mlp.predict(X_test)
-print(round(np.sum(np.equal(Y_pred_mlp, Y_test))/len(Y_test) * 100, 2))
+# Prediction sur les donnees de tests
+predic_test = mlp.predict(x_train)
 
-# Log perte (Log loss)
-print(log_loss(Y_pred_mlp, Y_test))
+# Affichage des donnees en fonction de leur classification
+# Affichage erreurs pour l'entrainement et les tests
+print("Erreur d'entrainement : ", (1 - mlp.score(x_train, t_train)) * 100, "%")
+print("Erreur de test : ", (1 - mlp.score(x_test, t_test)) * 100, "%")
 
+
+print("Meilleur hyperparametre : ", mlp.get_best_param())
+
+
+# Affichage matrice de confusion
 # Matrice de confusion
-print(confusion_matrix(Y_pred_mlp, Y_test, labels=[1,0]))
-sns.heatmap(confusion_matrix(Y_pred_mlp, Y_test), annot=True,lw =2,cbar=False)
-plt.ylabel("valeurs réelles")
-plt.xlabel("Valeurs prédites")
-plt.title("matrice de confusion")
+sns.heatmap(confusion_matrix(t_test, predic_test), annot=True, lw=2, cbar=False)
+plt.title("Matrice de confusion")
+plt.ylabel("Valeurs réelles")
+plt.xlabel("Valeurs prédis")
 plt.show()
-
 
 
